e993e43fe7f99bd760eada68999677df59ff11ff
Damion Walker <damionrwalker@gmail.com>
2015-08-24 23:33:47 -0400

    adding image to dashboard page and creating a simple form

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// set the current question to one
+cq = 1;
 
+function focusOn(el, id) {
+    $("."+el).addClass("h");
+    $("."+el+"#"+el+id).removeClass("h");
+}
+
+$(".submit").click(function(){
+  cq++;
+  focusOn("q", cq);
+  console.log(cq);
+});
+
+$(".b").click(function(){
+  var cl = $(this).parent().parent().attr("id");
+  var id = $(this).attr("id");
+  var mv = $("#"+cl+"#"+id).attr("data-m");
+  
+});
 
 
 
