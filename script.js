112f281f59c962c39db264221f878cd1b328e803
Damion Walker <damionrwalker@gmail.com>
2015-08-25 23:13:28 -0400

    created form  for dashboard page and added css styling to the page and also  added images to the home page

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // set the current question to one
-cq = 1;
 
-function focusOn(el, id) {
-    $("."+el).addClass("h");
-    $("."+el+"#"+el+id).removeClass("h");
-}
-
-$(".submit").click(function(){
-  cq++;
-  focusOn("q", cq);
-  console.log(cq);
-});
-
-$(".b").click(function(){
-  var cl = $(this).parent().parent().attr("id");
-  var id = $(this).attr("id");
-  var mv = $("#"+cl+"#"+id).attr("data-m");
-  
-});
 
 
 
